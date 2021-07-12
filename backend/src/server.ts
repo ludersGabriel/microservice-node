eb603415444ad6ee3609ac6f5ea442318840c0df
Gabriel Lüders <gabeluders@gmail.com>
2021-07-12 14:34:50 -0300

    eslint added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, resp) => {
-  return resp.json({message: 'Hello world'})
+  return resp.json({ message: 'Hello world' })
 })
 
 app.listen(4000)
