4eb8b804396c4a7536c57be757bb0a7815b2eab4
Gabriel Lüders <gabeluders@gmail.com>
2021-07-22 10:06:33 -0300

    cleanup and initial typescript setup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Template Repo</title>
+      </Head>
+
+      <main>
+        <div>This is a template Repo</div>        
+      </main>
+
+      
+    </div>
+  )
+}
