5091d2227cfb44fbf783fc288b43c086236ead15
Gabriel Lüders <gabeluders@gmail.com>
2021-07-22 10:40:49 -0300

    finished eslint setup

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home () {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -14,3 +14,5 @@ export default function Home () {
     </div>
   )
 }
+
+export default Home
