a44329856e41384f5bad452297dc361cbb5443af
Gabriel Lüders <gabeluders@gmail.com>
2021-07-22 13:42:20 -0300

    final structure?

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,23 @@
 import Head from 'next/head'
 
+import Walter from '../assets/walter.svg'
+import { Container } from '../styles/pages/home'
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Template Repo</title>
       </Head>
 
-      <main>
-        <h1>This is a template Repo</h1>
-      </main>
+      <Walter/>
+      <h1>NextJS and NodeJS simple structure</h1>
+      <p>
+        A NextJS + NodeJS template with integrated styled-components,
+        {' '}typescript and eslint
+      </p>
 
-    </div>
+    </Container>
   )
 }
 
